08fd844afd55cf208ef02d88171b0b0081a264d5
alexantonmota <57116717+alexantonmota@users.noreply.github.com>
2020-10-25 21:45:38 +0100

    Update GestionAdministrador.java

--- src/GestionAdministrador.java
+++ src/GestionAdministrador.java
@@ -8,26 +8,6 @@ public class GestionAdministrador {
 		public Administrador obteneradministrador(Administrador admin) {
 			Administrador administrador = null;
 			
-			Conexion conexion1 = new Conexion();
-			PreparedStatement pst = null;
-			ResultSet rs = null;
-			
-			try {
-				Connection cn1 = conexion1.conectar();
-				String sql = "select * from administrador where Username = ? and contr = ?";
-				pst = cn1.prepareStatement(sql);
-				pst.setString(1, admin.getUsername());
-				pst.setString(2, admin.getContraseña());
-				rs = pst.executeQuery();
-				
-				while(rs.next()) {
-					administrador = new Administrador(rs.getString(1), rs.getString(2));
-				}
-				
-			}catch (Exception e) {
-				System.out.println("Administrador no encontrado");
-			}
-			return administrador;
-		}
+		
 
 }

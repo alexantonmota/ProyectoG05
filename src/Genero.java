fc7716be301dd5ff6aab961d4701e328948055c7
alexantonmota <57116717+alexantonmota@users.noreply.github.com>
2020-10-31 18:40:34 +0100

    Delete Genero.java

--- src/Genero.java
+++
@@ -1,7 +0,0 @@
-
-public enum Genero {
-	TERROR,ACCION,COMEDIA,ROMANTICA,THRILLER,SCIFI,ANIME,HISTORIA,CLASICA,MUSICAL,DRAMA,INFANTIL;
-	static Genero getGenero(String elemento){
-        return Enum.valueOf(Genero.class,elemento);
-	}
-}
